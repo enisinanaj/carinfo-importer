ed8ade5cb0cfd0651ddd6d0793d6c615166f4bdd
Eni Sinanaj <enisinanaj@outlook.com>
2018-06-24 23:49:38 +0200

    Coverage

--- src/test/java/com/nlc/dataimporter/service/storage/FileNameValidatorTest.java
+++ src/test/java/com/nlc/dataimporter/service/storage/FileNameValidatorTest.java
@@ -3,8 +3,11 @@ package com.nlc.dataimporter.service.storage;
 import com.nlc.dataimporter.utils.TestUtils;
 import org.junit.Test;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class FileNameValidatorTest {
 
@@ -21,11 +24,17 @@ public class FileNameValidatorTest {
     @Test(expected=RuntimeException.class)
     public void givenFileWithEmptyFilename_whenValidating_ExceptionIsReturned() throws Exception {
         //given
-        MockMultipartFile file = new MockMultipartFile(null, null,
-                "text/plain", TestUtils.IMPORT_LINE_FOR_TEST.getBytes());
+        MultipartFile file = mock(MultipartFile.class);
+        given(file.getOriginalFilename()).willReturn(null);
 
         //when
         new FileNameValidator().validate(file);
+    }
+
+    @Test(expected=RuntimeException.class)
+    public void givenNullObject_whenValidating_ExceptionIsReturned() throws Exception {
+        //when
+        new FileNameValidator().validate(null);
     }
 
     @Test
